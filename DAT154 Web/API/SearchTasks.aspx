25a2d4f6b2933026a9c56e28169cfee08f3cf37e
Pål Gjerde <paalvaa@gmail.com>
2017-02-24 15:48:45 +0100

    Added task search API page

--- DAT154 Web/API/SearchTasks.aspx
+++ DAT154 Web/API/SearchTasks.aspx
@@ -1,16 +1,3 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SearchTasks.aspx.cs" Inherits="DAT154_Web.API.SearchTasks" %>
 
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-    <div>
-    
-    </div>
-    </form>
-</body>
-</html>
+<%= json %>
--- DAT154 Web/API/SearchTasks.aspx.cs
+++ DAT154 Web/API/SearchTasks.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAT154_Libs;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,35 @@ using System.Web.UI.WebControls;
 
 namespace DAT154_Web.API {
     public partial class SearchTasks : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
 
+        protected string json = "";
+        protected void Page_Load(object sender, EventArgs e) {
+            if (HttpContext.Current.Request.HttpMethod == "POST") {
+                string token = HttpContext.Current.Request["token"];
+                string room_id = HttpContext.Current.Request["room_id"];
+                string _category = HttpContext.Current.Request["category"];
+                string _status = HttpContext.Current.Request["status"];
+
+                if (Application["api:token-" + token] == null) {
+                    json = JsonConvert.SerializeObject(new { status = "ERROR", error = "Invalid user token." });
+                    return;
+                }
+
+                Room room = null;
+                if (room_id != null) room = Data.getRoomById(Convert.ToInt32(room_id));
+                int? status = null;
+                if (_status != null) status = Convert.ToInt32(_status);
+                int? category = null;
+                if (_category != null) category = Convert.ToInt32(_category);
+
+                List<Task> tasks = Data.getTasks(room: room, status: status, category: category);
+
+                json = JsonConvert.SerializeObject(new { status = "OK", tasks = tasks });
+
+            }
+            else {
+                json = JsonConvert.SerializeObject(new { status = "ERROR", error = "Request must be POST." });
+            }
         }
     }
 }
